1d5c97056c19c0fbe76abb769e27b810922272dd
Harald Klinke <hklinke@users.noreply.github.com>
2021-04-01 13:05:58 +0200

    Update create_tsne_plot.R

--- create_tsne_plot.R
+++ create_tsne_plot.R
@@ -1,11 +1,15 @@
-# Any questions of suggestions? Write me please.
+# Any questions of suggestions? Please write me.
 
 # Prerequisites_
 # Import data set from https://github.com/MuseumofModernArt/collection
 # Download images
 
-# Create set with features 
+library(Rtsne)
+library(imager)
+faktor = 500 #size of thumbnails
+pathtoimages=""
 
+# Create set with features from data frame
 art_sub = subset(
   Artworks,
   select = c(
@@ -32,29 +36,15 @@ art_sub$EndDate = as.numeric(substr(art_sub$EndDate, 2, 5))
 # tsne with only first 1000
 art_sub_use = art_sub_impute[1:1000, ]
 
-library(Rtsne)
-
-tsne_out = Rtsne(
-  art_sub_use[, 2:12],
-  dims = 2,
-  check_duplicates = FALSE,
-  theta = 0.4
-)
-
-# Create imageplot
-
-library(imager)
-faktor = 500 #size of thumbnails
-
-for (j in 1:100) {
-  message("Creating image #", j)
+# Crete t-SNE map
   tsne_out = Rtsne(
-    art_sub_use[, 2:12],
+    art_sub_use[, 1:12],
     dims = 2,
     check_duplicates = FALSE,
     theta = 0.4
   )
   
+# Create imageplot
   png(
     filename = paste0(format(Sys.time(), "%H%M%S"), "-tsne_MoMa.png"),
     width = 19200 / 20,
@@ -63,27 +53,27 @@ for (j in 1:100) {
     bg = "white"
   )
   
-  # Create empty plot
+# Create empty plot
   plot(   
     c(min(tsne_out$Y[, 1]), max(tsne_out$Y[, 1])),
-    c(min(tsne_out$Y[, 2]), max(tsne_out$Y[, 2])),
+    c(min(tsne_out$Y[, 1]), max(tsne_out$Y[, 1])),
     type = "n",
     axes = FALSE,
     xlab = "",
     ylab = "",
     main = "1.000 MoMA objects, t-sne imageplot (H. Klinke)",
-    family = "Open Sans",
     cex.main = 1
   )
-  
+
+# Place images
   for (i in (1:(nrow(tsne_out$Y) - 0))) {
-    #
     filename = paste(
-      "",    # path to images
+      pathtoimages,    
       art_sub_use$ObjectID[i],
       ".jpg",
       sep = ""
     )
+    
     if (file.exists(filename)) {
       image = load.image(filename)
     
@@ -99,5 +89,4 @@ for (j in 1:100) {
     }
   }
   dev.off()
-  
-} 
+ 
